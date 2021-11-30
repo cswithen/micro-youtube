1dacd7ab242e3cdf04ca3cd20f60b8d6fc831a86
cswithen <cody.swithenbank@gmail.com>
2021-11-30 09:30:51 -0800

    feat: imported search
    imported search component and started to use youtube-api-search module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 import { API_KEY } from "../secrets";
-
 import SearchBar from "./components/search_bar";
+
+YTSearch({ key: API_KEY, term: "surfboards" }, function (data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 const App = () => {
